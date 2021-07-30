9f9e57b7a7a95b90b4208d586af00bd99fbf616d
Pelmeshka102 <arsenii_tishkov@mail.ru>
2021-07-30 16:47:52 +0500

    dsfa

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import bridge from '@vkontakte/vk-bridge';
+// import bridge from '@vkontakte/vk-bridge';
 
 
-bridge.send("VKWebAppInit");
+vkBridge.send("VKWebAppInit");
 // Отправка события 
-bridge.send("VKWebAppGetUserInfo"); 
+vkBridge.send("VKWebAppGetUserInfo"); 
  // Подписка на событие-результат 
-bridge.subscribe((e) => { 
+ vkBridge.subscribe((e) => { 
 if(e.type == 'VKWebAppGetUserInfoResult') { 
    console.log(e.data.status); 
 }});
